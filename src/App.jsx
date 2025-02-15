e405a257a5d34ca1fdf69f9ed2537061fb6d5d7b
Zettex <safaandsafa4@gmail.com>
2025-02-15 22:14:48 +0600

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+const App = () =>{
+  return(
+    <div className="main">
+      Hello World
+    </div>
+  )
+}
+
+export default App;
